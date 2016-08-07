6626d647f0a4e20e537cbd61a09820ef36e2cff9
Frank Carver <frank@efsol.com>
2016-08-07 18:15:56 +0100

    capitalise title of All Tests in examples

--- examples/test_all.js
+++ examples/test_all.js
@@ -1,6 +1,6 @@
 var Hath = require('../index');
 
-module.exports = Hath.suite('All tests', [
+module.exports = Hath.suite('All Tests', [
   require('./test_example'),
   require('./test_exceptions'),
   require('./test_custom_assert')
